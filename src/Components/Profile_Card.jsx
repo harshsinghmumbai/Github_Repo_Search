1a8878be3f25c79e937cc29c2a11fd6966b687e4
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-05-25 22:42:15 +0530

    logical_part is implemented in project specifically fetch data and show in card.

--- src/Components/Profile_Card.jsx
+++ src/Components/Profile_Card.jsx
@@ -1,56 +1,72 @@
+"use client";
 import Image from "next/image";
 import { BsFillPostcardHeartFill } from "react-icons/bs";
 import { FaLocationDot } from "react-icons/fa6";
 
-const Profile_Card = () => {
+const Profile_Card = ({ repos }) => {
+  const {
+    avatar_url,
+    bio,
+    created_at,
+    id,
+    name,
+    public_repos,
+    location,
+    following,
+    followers,
+  } = repos;
+  console.log("repo existts", repos);
   return (
     <>
-      <div className="mt-10 sm:mt-2 flex flex-col">
+      <div className="mt-10 sm:mt-5 flex flex-col sm:flex sm:flex-row ">
         <div className="flex justify-center">
           <Image
-            src=""
+            src={avatar_url}
             alt="image"
-            className="border border-blue-500 rounded-full w-44 h-44"
+            className="border border-blue-500 rounded-full object-cover w-44 h-44 sm:w-[19rem] sm:h-48"
+            width={100}
+            height={100}
           />
         </div>
-        <div className="mt-3 flex justify-between">
-          <p className="text-sm sm:text-lg font-semibold font-serif capitalize">
-            harsh singh
+        <div className="sm:ml-5">
+          <div className="mt-3 flex justify-between">
+            <p className="text-sm sm:text-lg font-semibold font-serif capitalize">
+              {name}
+            </p>
+            <p className="text-sm sm:text-lg font-semibold font-serif text-center">
+              {created_at}
+            </p>
+          </div>
+          <p className="text-sm sm:text-lg font-serif capitalize my-2">
+            User_Id:- {id}
           </p>
-          <p className="text-sm sm:text-lg font-semibold font-serif text-center">
-            Joined_at :- 16/8/2023
+          <p className="text-left sm:text-xl sm:font-serif sm:text-center my-3">
+            {bio}
           </p>
-        </div>
-        <p className="text-sm sm:text-lg font-serif capitalize my-2">
-          User_Id:- 145204222
-        </p>
-        <p className="text-left sm:text-xl sm:font-semibold sm:text-center my-3">
-          I aim to become a skilled full Stack web developer as a student every
-          day.
-        </p>
-        <div className="bg-[#f6f8ff] rounded-xl w-full sm:w-[60%] m-auto flex justify-between p-2">
-          <p className="text-base sm:text-lg sm:font-semibold font-mono">
-            Repository
-            <span className="flex ml-5 ">43</span>
-          </p>
-          <p className="text-base sm:text-lg sm:font-semibold font-mono">
-            Follower
-            <span className="flex ml-5">0</span>
-          </p>
-          <p className="text-base sm:text-lg sm:font-semibold font-mono">
-            Following
-            <span className="flex ml-5">0</span>
-          </p>
-        </div>
-        <div className="w-full sm:w-[60%] m-auto mt-3 flex justify-evenly p-2 bg-[#fefce8]">
-          <p className="flex text-base sm:text-lg">
-            <BsFillPostcardHeartFill className="text-xl sm:text-3xl mr-3" />
-            40
-          </p>
-          <p className="flex text-base sm:text-lg capitalize">
-            <FaLocationDot className="text-xl sm:text-3xl mr-3" />
-            India
-          </p>
+          <div className="bg-[#f6f8ff] rounded-xl w-full sm:w-[90%] m-auto sm:m-0 flex justify-around p-2">
+            <p className="text-base font-medium sm:text-lg sm:font-semibold font-mono">
+              Repository
+              <span className="flex ml-5 ">{public_repos}</span>
+            </p>
+            <p className="text-base font-medium sm:text-lg sm:font-semibold font-mono">
+              Follower
+              <span className="flex ml-5">{followers}</span>
+            </p>
+            <p className="text-base font-medium sm:text-lg sm:font-semibold font-mono">
+              Following
+              <span className="flex ml-5">{following}</span>
+            </p>
+          </div>
+          <div className="w-full sm:w-[90%] m-auto sm:m-0 sm:my-3 mt-3 flex justify-evenly p-2 bg-[#fefce8]">
+            <p className="flex justify-center items-center text-base sm:text-lg">
+              <BsFillPostcardHeartFill className="text-xl sm:text-3xl mr-3" />
+              <span className="sm:text-lg"> {public_repos}</span>
+            </p>
+            <p className="flex justify-center items-center text-base sm:text-lg capitalize">
+              <FaLocationDot className="text-xl sm:text-3xl mr-1" />
+              <span className="sm:text-base"> {location}</span>
+            </p>
+          </div>
         </div>
       </div>
     </>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,29 @@
+"use client";
 import Profile_Card from "@/Components/Profile_Card";
 import { Button } from "@/Components/ui/button";
 import { Input } from "@/Components/ui/input";
+import { useEffect, useState } from "react";
 import { AiFillMoon } from "react-icons/ai";
 
 export default function Home() {
+  const [value, setvalue] = useState("harshsinghmumbai");
+  const [repos, setRepos] = useState({});
+
+  const getData = async () => {
+    try {
+      const url = `https://api.github.com/users/${value}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setRepos(data);
+    } catch (error) {
+      console.log("Error during fetching data", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
       <main className="grid place-items-center mt-10 sm:h-screen sm:mt-0">
@@ -16,13 +36,19 @@ export default function Home() {
               <AiFillMoon className="text-2xl" />
             </p>
           </nav>
-          <form className="flex mt-5 justify-center items-center">
-            <Input type="text" placeholder="Search" />
-            <Button size="sm" className="ml-3">
+          <div className="flex mt-5 justify-center items-center">
+            <Input
+              type="text"
+              placeholder="Search"
+              value={value}
+              className="text-base font-mono tracking-wider"
+              onChange={(e) => setvalue(e.target.value)}
+            />
+            <Button className="ml-3" type="submit" onClick={getData}>
               Search
             </Button>
-          </form>
-          <Profile_Card />
+          </div>
+          <Profile_Card repos={repos} />
         </div>
       </main>
     </>
